f7148154e8428f1d32789c1c7a1d40377294c64b
Anders Børjesson <anbo@zealand.dk>
2020-03-27 19:56:18 +0100

    simple calculator

--- calculator3/calculator3/MainPage.xaml.cs
+++ calculator3/calculator3/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace calculator3
 {
@@ -12,6 +7,42 @@ namespace calculator3
         public MainPage()
         {
             InitializeComponent();
+            ButtonCalculate.Clicked += (sender, args) =>
+            {
+                MessageLabel.Text = "Clicked ";
+                string number1Str = Number1Entry.Text;
+                string number2Str = Number2Entry.Text;
+                MessageLabel.Text += number1Str + " " + number2Str;
+                if (number1Str == null || number1Str.Trim() == "")
+                {
+                    MessageLabel.Text = "Not a number";
+                    return;
+                }
+                double number1 = double.Parse(number1Str);
+                double number2 = double.Parse(number2Str);
+                double result = 0.0;
+                int selectedIndex = OperationPicker.SelectedIndex;
+                MessageLabel.Text += " " + selectedIndex;
+                switch (selectedIndex)
+                {
+                    case -1:
+                        MessageLabel.Text = "Select an operation";
+                        return; // nothing selected
+                    case 0: // +
+                        result = number1 + number2;
+                        break;
+                    case 1: // -
+                        result = number1 - number2;
+                        break;
+                    case 2: // *
+                        result = number1 * number2;
+                        break;
+                    case 3: // *
+                        result = number1 / number2;
+                        break;
+                }
+                ResultLabel.Text = result.ToString();
+            };
         }
     }
 }
